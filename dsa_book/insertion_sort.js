a9e2f188bb96c1da7748198702f024afb922f327
Aart Fuhler <aart.fuhler@gmail.com>
2025-10-27 17:39:47 -0500

    added insertion sort

---
+++ dsa_book/insertion_sort.js
@@ -0,0 +1,27 @@
+function insertionSort(arr) {
+  const len = arr.length;
+
+  // Iterate over the array starting from the second element
+  for (let i = 1; i < len; i++) {
+    //Store the current element, creating a 'gap' at index `i`
+    let current = arr[i];
+    // Set the index to compare against to the left of our 'gap'
+    let j = i - 1;
+
+    // Keep shifting as long as the element we are comparing to
+    // is greater than the currently stored element, or we run out of elements
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    // Insert the stored element into the gap
+    arr[j + 1] = current;
+  }
+
+  return arr;
+}
+
+const array = [4, 2, 1, 3]; // [2, 4, 1, 3] [1, 2, 4, 3]
+console.log(insertionSort(array));
