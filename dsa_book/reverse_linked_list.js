0ba1ad2973fdc3fbcdd0b11224182ee277fd3ff4
Aart Fuhler <aart.fuhler@gmail.com>
2025-10-31 17:14:39 -0500

    added reverse_linked_list solution

---
+++ dsa_book/reverse_linked_list.js
@@ -0,0 +1,45 @@
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function reverseLinkedList(head) {
+  if (!head) return head;
+
+  let curr = head;
+  let prev = null;
+  let nextNode = curr.next;
+
+  while (curr !== null) {
+    let nextNode = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+
+  return prev;
+}
+
+function printList(head) {
+  let curr = head;
+  let result = "";
+  while (curr !== null) {
+    result += curr.val + " -> ";
+    curr = curr.next;
+  }
+  result += "null";
+  return result;
+}
+
+// Test case 1
+const head1 = new ListNode(1);
+head1.next = new ListNode(2);
+head1.next.next = new ListNode(3);
+head1.next.next.next = new ListNode(4);
+
+console.log("Input: ", printList(head1));
+console.log("Output:", printList(reverseLinkedList(head1)));
+// Input:  1 -> 2 -> 3 -> 4 -> null
+// Output: 4 -> 3 -> 2 -> 1 -> null
