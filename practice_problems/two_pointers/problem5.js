59fa01e5ad919d319c00ec473a860644c0f059d2
Aart Fuhler <aart.fuhler@gmail.com>
2025-11-18 15:42:06 -0600

    added two_pointers problem 5

---
+++ practice_problems/two_pointers/problem5.js
@@ -0,0 +1,32 @@
+function countPairs(nums, target) {
+  let pairCount = 0;
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start < end) {
+    const currentSum = nums[start] + nums[end];
+    if (currentSum > target) {
+      pairCount += end - start;
+      end--;
+    } else {
+      start++;
+    }
+  }
+
+  return pairCount;
+}
+
+console.log(countPairs([1, 2, 3, 4, 5], 6) === 4);
+// Pairs: (2, 5), (3, 4), (3, 5), (4, 5)
+
+console.log(countPairs([1, 2, 3, 4, 5], 8) === 1);
+// Pair: (4, 5)
+
+console.log(countPairs([1, 3, 5, 7], 6) === 4);
+// Pairs: (1, 7), (3, 5), (3, 7), (5, 7)
+
+console.log(countPairs([1, 2, 3, 4], 5) === 2);
+// Pairs: (2, 4), (3, 4)
+
+console.log(countPairs([1, 2, 3, 4, 5], 10) === 0);
+// No pairs
