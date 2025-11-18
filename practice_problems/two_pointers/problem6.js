c8bba5984178f0cd651a562d355f43df70020f26
Aart Fuhler <aart.fuhler@gmail.com>
2025-11-18 15:42:12 -0600

    added two_pointers problem 6

---
+++ practice_problems/two_pointers/problem6.js
@@ -0,0 +1,28 @@
+function checkTripleMatch(nums) {
+  let anchor = 0;
+  let runner = 1;
+
+  while (runner < nums.length) {
+    const product = nums[anchor] * 3;
+    if (product === nums[runner]) {
+      return true;
+    } else if (product < nums[runner]) {
+      anchor++;
+      runner++;
+    } else {
+      runner++;
+    }
+  }
+
+  return false;
+}
+
+console.log(checkTripleMatch([1, 3, 9, 28]) === true);
+console.log(checkTripleMatch([1, 2, 4, 10, 11, 12]) === true);
+console.log(checkTripleMatch([0, 5, 7, 55]) === false);
+console.log(checkTripleMatch([4, 5, 7, 9, 13, 15, 17]) === true);
+console.log(checkTripleMatch([2, 6, 13, 54]) === true);
+console.log(checkTripleMatch([1, 5, 17, 51]) === true);
+console.log(checkTripleMatch([1, 2, 4, 8]) === false);
+
+// All test cases should log true.
