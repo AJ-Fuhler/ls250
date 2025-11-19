f1019321cd46d5578992f0dfa597a166dcce0b2a
Aart Fuhler <aart.fuhler@gmail.com>
2025-11-19 16:23:35 -0600

    added recursion exercises

---
+++ practice_problems/recursion/problem3.js
@@ -0,0 +1,14 @@
+function countDigits(n) {
+  if (n < 10) {
+    return 1;
+  }
+  return 1 + countDigits(Math.floor(n / 10));
+}
+
+console.log(countDigits(12345) === 5);
+console.log(countDigits(7) === 1);
+console.log(countDigits(100000) === 6);
+console.log(countDigits(99999) === 5);
+console.log(countDigits(0) === 1);
+
+// All test cases should log true.
